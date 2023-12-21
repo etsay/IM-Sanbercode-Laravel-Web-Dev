2766ac5324705b6dca0e39296d9b0d25ca44d077
Youretsay <129036330+etsay@users.noreply.github.com>
2023-12-21 19:02:38 +0700

    Update Ape.php

--- Tugas 9 - OOP PHP/Ape.php
+++ Tugas 9 - OOP PHP/Ape.php
@@ -6,7 +6,7 @@ class Ape extends Animal {
     public $legs = 2; // Mengubah properti legs menjadi 2 pada class Ape
 
     public function yell() {
-        echo "Yell : Auooo" . PHP_EOL;
+        echo "Yell : Auooo";
     }
 }
 
